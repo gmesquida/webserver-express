9f2245df3521824bbde07ec945a8d05d4d445006
“Guillermo” <gmesquida@gmail.com>
2021-01-15 12:11:33 +0100

    Correción fallo de port en heroku

--- express.js
+++ express.js
@@ -4,7 +4,7 @@ const app = express()
 
 require('./hbs/helpers')
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000;
 
 // Publicamos el archivo estático
 app.use(express.static(__dirname + '/public'))
